462a4778d60480f8e202f8878ceccb409c4fdb30
tomas021221 <tomasdanielmorato@gmail.com>
2025-05-28 17:21:33 -0300

    Add files via upload

---
+++ promedioconfunciones(void).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int calcularpromedio(float sumatoria, int cantnotas){
+	return sumatoria/ cantnotas;
+}
+
+	void imprimirnotas(int cantnotas, float promedio) {
+		printf("***** Resultado de las notas ingresadas *****\n");
+		printf("Cantidad de notas procesadas: %d\n", cantnotas);
+		printf("Promedio: %.2f\n", promedio);
+		printf("**********************************************\n");
+	}
+	
+int main(int argc, char *argv[]) {
+	float notas, promedio, acumul=0;
+	int cantnotas=3;
+	
+	for (int i=0; i< cantnotas; i++){
+		printf("Ingrese la nota: \n");
+		scanf("%f", &notas);
+		acumul+=notas;
+	}
+	
+	promedio=calcularpromedio(acumul, cantnotas);
+	
+	imprimirnotas(cantnotas, promedio);
+
+	return 0;
+}
+	
